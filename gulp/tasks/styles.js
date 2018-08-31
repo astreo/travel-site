7f183d5fb1d3be671fb77f23ba6789d48da8f543
astreo <rodrigo.astrada@gmail.com>
2018-08-31 08:33:20 -0400

    mixins y wrapper

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,16 @@ var gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins'),
     extname = require('gulp-extname');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.pcss')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
         .pipe(extname('.css'))
         .pipe(gulp.dest('./app/temp/styles'));
 });
